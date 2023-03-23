1abd750d70f735d12a70d637f797db22ef3e7bf6
Admin <Admin@DESKTOP-0C3M1BG>
2023-03-23 23:34:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using constructor5;
+
+public static class programm
+{
+
+    public static void Main()
+    {
+        doctor doctor1= new doctor();
+        
+        doctor doctor2 = new doctor(11, "swapnil","mbbs");
+        doctor doctor3 = new doctor(12, " arjun", "bds");
+        doctor doctor4 = new doctor(doctor3);
+
+        doctor1.showdeta();
+        doctor2.showdeta();
+        doctor3.showdeta();
+        doctor4.showdeta();
+       
+
+    }
+}
---
+++ doctor5.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor5
+{
+    internal class doctor
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string degree { get; set; }
+
+        public static int number { get; set; } 
+
+
+        public doctor()
+
+        {
+            this.id = 10;
+            this.name = "arun";
+            this.degree = "bams";
+
+        }
+
+        public doctor(int id, string name, string degree)
+
+        {
+            this.id = id;
+            this.name = name;
+            this.degree = degree;
+
+        }
+        public doctor(doctor doctor)
+        {
+
+            this.id = doctor.id;
+            this.name = doctor.name;
+            this.degree =doctor. degree;
+
+
+        }
+        static doctor()
+        {
+            doctor.number = 4;
+        }
+
+        public void showdeta()
+        {
+            Console.WriteLine("id=" + this.id);
+            Console.WriteLine("name=" + this.name);
+            Console.WriteLine("degree= " + this.degree);
+
+
+        }
+
+
+
+    }
+}
+
